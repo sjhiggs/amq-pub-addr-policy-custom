41c0131d024568bd956d2267cd44bbd3cc5423d1
Stephen Higgs <shiggs@redhat.com>
2016-08-08 08:55:58 -0400

    cleanup, documentation

---
+++ Readme.md
@@ -0,0 +1,36 @@
+Custom Published Address Strategy
+=================================
+
+
+Version
+-------
+This modification was originally developed and compiled for A-MQ 6.2.1-117.  Modify the pom.xml dependency as required for your installation.
+
+Compile
+-------
+mvn clean install
+
+
+Install
+-------
+JBossA-MQ:karaf@root> osgi:install mvn:com.redhat.examples/my-published-address-strategy/1.0.0-SNAPSHOT
+JBossA-MQ:karaf@root> la | grep activemq-osgi
+[ 137] [Active     ] [Created     ] [       ] [   50] activemq-osgi (5.11.0.redhat-621117), Fragments: 153
+JBossA-MQ:karaf@root> update 137
+You are about to access system bundle 137.  Do you wish to continue (yes/no): yes
+
+
+Modify etc/activemq.xml
+-----------------------
+<bean class="com.redhat.examples.broker.PublishedAddressPolicyHostnameConfig" id="addressPolicy">
+        <property name="hostname" value="myamq-a-public.local" />
+</bean>
+...
+<transportConnectors>
+            <transportConnector name="openwire" publishedAddressPolicy="#addressPolicy"  uri="tcp://0.0.0.0:0?maximumConnections=1000&amp;wireFormat.maxFrameSize=104857600" updateClusterClients="true" rebalanceClusterClients="true" updateClusterClientsOnRemove="true"/>
+</transportConnectors>
+
+
+Restart Container
+-----------------
+Restart the container to pick up the modified etc/activemq.xml
--- src/main/java/com/redhat/examples/broker/PublishedAddressPolicyHostnameConfig.java
+++ src/main/java/com/redhat/examples/broker/PublishedAddressPolicyHostnameConfig.java
@@ -1,28 +1,61 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one or more
+ * contributor license agreements.  See the NOTICE file distributed with
+ * this work for additional information regarding copyright ownership.
+ * The ASF licenses this file to You under the Apache License, Version 2.0
+ * (the "License"); you may not use this file except in compliance with
+ * the License.  You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package com.redhat.examples.broker;
 
 import java.net.UnknownHostException;
-
 import org.apache.activemq.broker.PublishedAddressPolicy;
 import org.apache.log4j.Logger;
 
+/**
+ * Allows the broker's hostname to be configured. If a hostname is configured,
+ * it will be used, otherwise the policy will revert to the default behavior
+ * found in PublishedAddressPolicy.
+ */
 public class PublishedAddressPolicyHostnameConfig extends PublishedAddressPolicy {
 
-	private String hostname;
-	
-	private Logger logger = Logger.getLogger(PublishedAddressPolicyHostnameConfig.class.getName());
-	
-	public void setHostname(String hostname) {
-		logger.trace("using configured hostname for published address policy: " + hostname);
-		this.hostname = hostname;
-	}
-	
-	public String getHostname() {
-		return hostname;
-	}
-	
-	@Override
-	protected String getPublishedHostValue(String val) throws UnknownHostException {
-		return (hostname == null) ? super.getPublishedHostValue(val) : hostname;
-	}
-	
+    private String hostname;
+
+    private Logger logger = Logger.getLogger(PublishedAddressPolicyHostnameConfig.class.getName());
+
+    /**
+     * Set/Configure the hostname to be returned by the broker when publishing
+     * the broker's hostname.
+     * 
+     * @param hostname
+     *            The user-specified hostname to be published
+     */
+    public void setHostname(String hostname) {
+        logger.trace("using configured hostname for published address policy: " + hostname);
+        this.hostname = hostname;
+    }
+
+    /**
+     * @return a String hostname value if one was configured, null otherwise
+     */
+    public String getHostname() {
+        return hostname;
+    }
+
+    /**
+     * Returns the configured hostname, or reverts to the parent's default
+     * behavior if a hostname has not been configured.
+     */
+    protected String getPublishedHostValue(String val) throws UnknownHostException {
+        return (hostname == null) ? super.getPublishedHostValue(val) : hostname;
+    }
+
 }
